e64dec7db7ceae37207c0b83520a218a9346ccd4
Hamiltonspjunior <hamiltonspjunior@gmail.com>
2019-03-24 17:22:12 -0300

    Conexao com o banco e funçao post e get

--- server.js
+++ server.js
@@ -1,6 +1,45 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-app.listen(3000, function () {
-    console.log('Server running on port 3000');
-});
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = "mongodb+srv://admin:Ifsp2019@cluster0-re2z0.mongodb.net/test?retryWrites=true"
+
+MongoClient.connect(uri, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('crud-nodejs') // coloque o nome do seu DB
+
+    app.listen(3000, () => {
+        console.log('Server running on port 3000')
+    })
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/', (req, res) => {
+    var cursor = db.collection('data').find()
+})
+
+app.get('/show', (req, res) => {
+    db.collection('data').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('show.ejs', { data: results })
+
+    })
+})
+
+app.post('/show', (req, res) => {
+    db.collection('data').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('Salvo no Banco de Dados')
+        res.redirect('/show')
+    })
+})
